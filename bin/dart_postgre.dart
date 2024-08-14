8efa07c74c8b191d935c9d26c1fa209703d9b701
Your Name <your_email@example.com>
2024-08-14 11:45:24 +0300

    first commit

---
+++ bin/dart_postgre.dart
@@ -0,0 +1,5 @@
+import 'package:dart_postgre/dart_postgre.dart' as dart_postgre;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_postgre.calculate()}!');
+}
---
+++ test/dart_postgre_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_postgre/dart_postgre.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
